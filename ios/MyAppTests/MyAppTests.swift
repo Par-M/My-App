b78562bda39df8626779341dfa6f58713fea5321
Par-M <fvvzhcqmyh@privaterelay.appleid.com>
2026-07-18 20:18:04 -0400

    Create SwiftUI Project and set up the backend

---
+++ ios/MyAppTests/MyAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  MyAppTests.swift
+//  MyAppTests
+//
+//  Created by Parthiv Manj on 2026-07-18.
+//
+
+import Testing
+
+struct MyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
